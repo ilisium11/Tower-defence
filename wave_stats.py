096edf1e884aed5b4eb3015fb72b7af846fa06c3
ilisium11 <120244737+ilisium11@users.noreply.github.com>
2022-12-10 23:24:01 +0500

    Update wave_stats.py

--- wave_stats.py
+++ wave_stats.py
@@ -86,35 +86,22 @@ class Levels:
         roads=[]
         road = LevelLoading(road_list)
         road.no_wave()
-        road.chaotic((Enemy1,1),True,4,80,200),road.chaotic((Enemy1,2),True,4,40,200),road.chaotic((Enemy1,3),True,4,80,200),road.create_new_wave()
-        road.chaotic((Enemy2,1),True,4,80,200), road.chaotic((Enemy2,2),True,4,80,200), road.chaotic((Enemy2,3),True,4,80,200),road.create_new_wave()
-        road.chaotic((Enemy3, 1), True, 4, 80, 200), road.chaotic((Enemy3, 2), True, 4, 80, 200), road.chaotic((Enemy3, 3), True, 4, 80, 200), road.create_new_wave()
-        road.chaotic((Enemy4, 1), True, 4, 80, 200), road.chaotic((Enemy4, 2), True, 4, 80, 200), road.chaotic((Enemy4, 3), True, 4, 80, 200), road.create_new_wave()
-        road.chaotic((Enemy5, 1), True, 4, 80, 200), road.chaotic((Enemy5, 2), True, 4, 80, 200), road.chaotic((Enemy5, 3), True, 4, 80, 200), road.create_new_wave()
+        road.chaotic((Enemy1,1),False,10,150,500),road.chaotic((Enemy2,1),False,3,40,200),road.chaotic((Enemy3,1),False,4,80,1),road.create_new_wave()
+        road.chaotic((Enemy2,1),False,10,300,200), road.chaotic((Enemy3,1),False,10,100,1),road.create_new_wave()
+        road.two_lines((Enemy1, 1), False,0.25, 20, 100, 400), road.chaotic((Enemy1, 1), False, 5, 350, 1), road.create_new_wave()
+        road.two_lines((Enemy1, 1), True, 0.25, 20, 150, 400), road.chaotic((Enemy3, 1), True, 5, 350,1), road.chaotic((Enemy2, 1), True, 1, 350,1), road.create_new_wave()
+        road.chaotic((Enemy4, 1), False, 1, 150, 50),road.chaotic((Enemy1, 1), True, 30, 100, 300),road.chaotic((Enemy4, 1), False, 1, 150, 50),road.chaotic((Enemy1, 1), True, 30, 100, 1), road.create_new_wave()
+        road.two_lines((Enemy3, 1), True,0.25, 50, 50, 150),road.two_lines((Enemy1, 2), False,0.25, 4, 50, 50),road.chaotic((Enemy2,2),False,5,400,1),road.create_new_wave()
+        road.chaotic((Enemy1, 2), True, 20, 250, 150),road.two_lines((Enemy4,1),0.25,True,1,1,20),road.chaotic((Enemy5,1),True,1,100,200),road.chaotic((Enemy1,2),True,30,100,200),road.create_new_wave()
+        road.chaotic((Enemy4,2),True,1,20,200),road.chaotic((Enemy5, 2), True, 1, 100, 150), road.chaotic((Enemy3,2),True,50,40,100),road.create_new_wave()
+        road.chaotic((Enemy1, 3), False, 30, 150, 200), road.chaotic((Enemy3, 3), False, 1, 100, 300), road.chaotic((Enemy2, 3), False, 15, 250, 100), road.create_new_wave()
+        road.two_lines((Enemy2, 3), True,0.25, 20, 150, 200), road.chaotic((Enemy4,3),True,1,30,30),road.chaotic((Enemy5,3),True,1,100,150), road.create_new_wave()
+        road.chaotic((Enemy4, 3), True, 2, 30, 30), road.chaotic((Enemy5, 3),True,2,30,250),road.chaotic((Enemy3,3),True,50,50,100), road.create_new_wave()
         roads.append(WaveLoading(road.waves_list))
-        delay_level=[1000,500,500,500,500,500]
+        delay_level=[100,100,100,100,100,100,100,100]
         health=1000
-        money=500000
+        money=700
         return health,money,roads,delay_level,closed_cells,bushes,stones
-
-    @staticmethod
-    def level2():
-        width = Levels.width_cell
-        road_list = [[width * 10, 0], [width * 10, width * 12], [width * 3, width * 12], [width * 3, width * 20]]
-        for i in range(len(road_list)):
-            road_list[i][0] = round(road_list[i][0], 1)
-            road_list[i][1] = round(road_list[i][1], 1)
-        wave = [0, [2, (Enemy1, False), 20, 200, 100], [2, (Enemy2, True), 1, 24, 100]]
-        delay_level = [1200]
-        waves1=[]
-        waves1.append(wave)
-        waves = []
-        waves.append(waves1)
-        path_list = [road_list]
-        health=10
-        money=1000
-        max_level_tower=2
-        return health,money,max_level_tower,waves,delay_level,path_list
 
     @staticmethod
     def draw_road(window,path_list):
@@ -146,14 +133,14 @@ class Levels:
     def create_bush(bush_cells):
         bushes=[]
         for bush_cell in bush_cells:
-            bushes.append(ObjTerrain(Levels.bush,'Куст',100,(bush_cell[0]*Levels.width_cell,bush_cell[1]*Levels.width_cell),bush_cell))
+            bushes.append(ObjTerrain(Levels.bush,'Куст',200,(bush_cell[0]*Levels.width_cell,bush_cell[1]*Levels.width_cell),bush_cell))
         return bushes
 
     @staticmethod
     def create_stone(stone_cells):
         stones=[]
         for stone_cell in stone_cells:
-            stones.append(ObjTerrain(Levels.stone,'Куст',100,(stone_cell[0]*Levels.width_cell,stone_cell[1]*Levels.width_cell),stone_cell))
+            stones.append(ObjTerrain(Levels.stone,'Куст',400,(stone_cell[0]*Levels.width_cell,stone_cell[1]*Levels.width_cell),stone_cell))
         return stones
 
     @staticmethod
