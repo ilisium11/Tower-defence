b311d4796bbd85b7aa88b190d144a42ebb922c01
ilisium11 <120244737+ilisium11@users.noreply.github.com>
2022-12-11 19:01:55 +0500

    Update wave_stats.py

--- wave_stats.py
+++ wave_stats.py
@@ -86,21 +86,21 @@ class Levels:
         roads=[]
         road = LevelLoading(road_list)
         road.no_wave()
-        road.chaotic((Enemy1,1),False,10,150,500),road.chaotic((Enemy2,1),False,3,40,200),road.chaotic((Enemy3,1),False,4,80,1),road.create_new_wave()
-        road.chaotic((Enemy2,1),False,10,300,200), road.chaotic((Enemy3,1),False,10,100,1),road.create_new_wave()
-        road.two_lines((Enemy1, 1), False,0.25, 20, 100, 400), road.chaotic((Enemy1, 1), False, 5, 350, 1), road.create_new_wave()
-        road.two_lines((Enemy1, 1), True, 0.25, 20, 150, 400), road.chaotic((Enemy3, 1), True, 5, 350,1), road.chaotic((Enemy2, 1), True, 1, 350,1), road.create_new_wave()
-        road.chaotic((Enemy4, 1), False, 1, 150, 50),road.chaotic((Enemy1, 1), True, 30, 100, 300),road.chaotic((Enemy4, 1), False, 1, 150, 50),road.chaotic((Enemy1, 1), True, 30, 100, 1), road.create_new_wave()
-        road.two_lines((Enemy3, 1), True,0.25, 50, 50, 150),road.two_lines((Enemy1, 2), False,0.25, 4, 50, 50),road.chaotic((Enemy2,2),False,5,400,1),road.create_new_wave()
+        road.chaotic((Enemy1,1),False,10,150,100),road.chaotic((Enemy2,1),False,10,40,200),road.chaotic((Enemy3,1),False,4,80,1),road.create_new_wave()
+        road.chaotic((Enemy2,1),False,10,200,200), road.chaotic((Enemy3,1),True,15,40,1),road.create_new_wave()
+        road.two_lines((Enemy1, 1), True,0.25, 20, 100, 400), road.two_lines((Enemy2, 1), False,0.25, 10, 90, 1), road.create_new_wave()
+        road.chaotic((Enemy4,1),True,1,100,1),road.two_lines((Enemy1, 2), False, 0.25, 20, 150, 400), road.chaotic((Enemy3, 1), True, 5, 350,1), road.chaotic((Enemy2, 1), True, 1, 350,1), road.create_new_wave()
+        road.chaotic((Enemy4, 1), False, 1, 150, 50),road.chaotic((Enemy1, 1), True, 30, 100, 300),road.chaotic((Enemy4, 1), False, 1, 150, 50),road.chaotic((Enemy2, 1), True, 10, 100, 1), road.create_new_wave()
+        road.two_lines((Enemy3, 1), True,0.25, 30, 50, 150),road.two_lines((Enemy1, 2), True,0.25, 6, 50, 50),road.chaotic((Enemy2,2),False,5,200,1),road.create_new_wave()
         road.chaotic((Enemy1, 2), True, 20, 250, 150),road.chaotic((Enemy4,1),True,2,1,20),road.chaotic((Enemy5,1),True,1,100,200),road.chaotic((Enemy1,2),True,30,100,200),road.create_new_wave()
-        road.chaotic((Enemy4,2),True,1,20,200),road.chaotic((Enemy5, 2), True, 1, 100, 150), road.chaotic((Enemy3,2),True,50,40,100),road.create_new_wave()
+        road.chaotic((Enemy4,2),True,1,20,200),road.two_lines((Enemy2,2),False,0.25,10,100,200),road.chaotic((Enemy5, 2), True, 1, 100, 150), road.chaotic((Enemy3,2),True,50,40,100),road.create_new_wave()
         road.chaotic((Enemy1, 3), False, 30, 150, 200), road.chaotic((Enemy3, 3), False, 1, 100, 300), road.chaotic((Enemy2, 3), False, 15, 250, 100), road.create_new_wave()
-        road.chaotic((Enemy2, 3), True, 20, 150, 200), road.chaotic((Enemy4,3),True,1,30,30),road.chaotic((Enemy5,3),True,1,100,150), road.create_new_wave()
-        road.chaotic((Enemy4, 3), True, 2, 30, 30), road.chaotic((Enemy5, 3),True,2,30,250),road.chaotic((Enemy3,3),True,50,50,100), road.create_new_wave()
+        road.chaotic((Enemy2, 3), True, 20, 150, 200), road.chaotic((Enemy4,3),True,1,30,30),road.chaotic((Enemy5,3),True,1,100,150),road.chaotic((Enemy1, 3),False,20,40,1), road.create_new_wave()
+        road.two_lines((Enemy2,3),True,0.25,10,100,500),road.chaotic((Enemy4, 3), True, 2, 30, 30), road.chaotic((Enemy5, 3),True,2,30,250),road.chaotic((Enemy3,3),True,60,50,100), road.create_new_wave()
         roads.append(WaveLoading(road.waves_list))
         delay_level=[600,600,600,600,600,600,600,600,600,600,600,600]
         health=20
-        money=700
+        money=800
         return health,money,roads,delay_level,closed_cells,bushes,stones
 
     @staticmethod
